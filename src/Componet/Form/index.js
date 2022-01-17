dfaee1bbb02344e3b23da43d9431e065ef7f4ff0
jenish <jenish@m.com>
2022-01-17 11:50:49 +0530

    new

--- src/Componet/Form/index.js
+++ src/Componet/Form/index.js
@@ -17,7 +17,7 @@ const loginC = [{
 {
     "Name": "Jagat Vasveliya",
     "Email": "jagat0@gmail.com",
-    "Password": "hey@jagat"
+    "Password": "123"
 },
 {
     "Name": "Arshit Khoshiya",
@@ -31,27 +31,41 @@ const validation = () => {
     let gemail = document.getElementById("mail").value;
     let password = document.getElementById("passw").value;
 
-    for (let index = 0; index < loginC.length; index++) 
-    {
-        if (loginC[index]['Email'] == gemail)
-         {
+    for (let index = 0; index < loginC.length; index++) {
+        if (gemail != "" || password != "") 
+        {
+            if (loginC[index]['Email'] === gemail) {
 
-            if (loginC[index]['Password'] == password) {
+                if (loginC[index]['Password'] === password) {
 
-                document.getElementById("mainfc").innerHTML = `<h1>Welcome<br /> ${loginC[index]["Name"]} </h1>`;
-                
-                break;
+                    document.getElementById("mainfc").innerHTML = `<h1> ${loginC[index]["Name"]} </h1>`;
+                    break;
 
+                }
+                else {
+                    document.getElementById('wom').style.display = "none";
+                    document.getElementById('wop').style.display = "block";
+                    //alert("wop");
+                    break;
+                }
             } else {
-                //alert("not found")
-                document.getElementById("mainfc").innerHTML = `<h1>Enter valid data</h1>`;
+                document.getElementById('wom').style.display = "block";
+                document.getElementById('wop').style.display = "none";
+
             }
-        } 
-       
+        } else{
+            //alert("not blnck")
+            document.getElementById("notkhali").style.display = "block";
+            document.getElementById("notkhali").style.display = "block";
+            break;
+        }
+
     }
 
 
 };
+
+
 
 
 export default class indexf extends Component {
@@ -63,15 +77,20 @@ export default class indexf extends Component {
                         <div className="m">
                             <label className=" ff">Email address:</label>
                             <input type="email" className="email" id='mail' />
+                            <div id='wom'>Email is not valid</div>
 
                         </div>
                         <div className="b">
-                            <label className=" ff">Password:</label>
-                            <input type="password" className="psaa" id='passw' />
+                            <label className=" ff" id='wp'>Password:</label>
+                            <input type="password" className="psaa" id='passw' /><br />
+                            <div id='wop'>Password is not valid</div>
                         </div>
 
+                        <div id='notkhali'>Not Blank</div>
+
+
                         <button type="button" className="btn btn-primary" onClick={validation}>Submit</button>
-                        
+
 
                     </div>
                 </form>
